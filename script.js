fee4293c587c9986d1a953295cac55201a40c011
Nelson Chen <crazysim@gmail.com>
2012-07-01 02:58:32 -0700

    only auto login on timeout

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // $$('input:-webkit-autofill')
 
-if (($('input:-webkit-autofill').length > 0) && ($('span[class="error"]').length == 0)){
-    $("form:first[method='post']").submit();
+if ($('input:-webkit-autofill').length > 0){
+    // if ($('span[class="error"]').length == 0)){
+        // $("form:first[method='post']").submit();
+    // }
 }
 
