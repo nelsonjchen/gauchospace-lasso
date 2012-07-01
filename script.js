d54072ab37b458c82726f29c566c294f12dc57d6
Nelson Chen <crazysim@gmail.com>
2012-07-01 09:42:00 -0700

    purpose

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // $$('input:-webkit-autofill')
 
 new (function(window, document, $){
+
+    // Auto relogin if timed out.
     if ($('input:-webkit-autofill').length > 0){
         // <span class="error">Your session has timed out.  Please login
         // again.</span>
         var error_msg = $('span[class="error"]').first();
-        if (error_msg.text().search("Please login again.")){
+        if (error_msg.text().search("Please login again.") > 0){
             $("form:first[method='post']").submit();
         }
     }
