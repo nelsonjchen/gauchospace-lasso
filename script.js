03c637f308eaf64936fb6e982ecf0497a4c896da
Nelson Chen <crazysim@gmail.com>
2012-07-01 09:56:57 -0700

    only run on login page

--- script.js
+++ script.js
@@ -3,14 +3,16 @@
 new (function(window, document, $){
 
     // Auto relogin if timed out.
-    if ($('input:-webkit-autofill').length > 0){
-        // <span class="error">Your session has timed out.  Please login
-        // again.</span>
-        var error_msg = $('span[class="error"]').first();
-        if (error_msg.text().search("Please login again.") > 0){
-            $("form:first[method='post']").submit();
+    if (window.location.pathname == "/courses/login/index.php"){
+        if ($('input:-webkit-autofill').length > 0){
+            // <span class="error">Your session has timed out.  Please login
+            // again.</span>
+            var error_msg = $('span[class="error"]').first();
+            if (error_msg.text().search("Please login again.") > 0){
+                $("form:first[method='post']").submit();
+            }
         }
-    }
 
+    }
 })(window, document, jQuery);
 
