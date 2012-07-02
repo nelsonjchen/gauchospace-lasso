d75528618b63dfaab50894735c725de9495bbbd1
Nelson Chen <crazysim@gmail.com>
2012-07-02 02:16:27 -0700

    run on document ready

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 // $$('input:-webkit-autofill')
 
 new (function(window, document, $){
-
-    // Auto relogin if timed out.
-    if (window.location.pathname == "/courses/login/index.php"){
-        if ($('input:-webkit-autofill').length > 0){
-            // <span class="error">Your session has timed out.  Please login
-            // again.</span>
-            var error_msg = $('span[class="error"]').first();
-            if (error_msg.text().search("Please login again.") > 0){
-                $("form:first[method='post']").submit();
+    $(document).ready(function(){    
+        // Auto relogin if timed out.
+        if (window.location.pathname == "/courses/login/index.php"){
+            if ($('input:-webkit-autofill').length > 0){
+                // <span class="error">Your session has timed out.  Please login
+                // again.</span>
+                var error_msg = $('span[class="error"]').first();
+                if (error_msg.text().search("Please login again.") > 0){
+                    $("form:first[method='post']").submit();
+                }
             }
         }
-    }
 
-    // Psuedodirectlinkify all document URLs
-    if (window.location.pathname == "/courses/course/view.php"){        
-        // Find all onclick elements in anchor links of such elements and
-        // remove them. Append &inpopup=true to all of them.
+        // Psuedodirectlinkify all document URLs
+        if (window.location.pathname == "/courses/course/view.php"){        
+            // Find all onclick elements in anchor links of such elements and
+            // remove them. Append &inpopup=true to all of them.
+            //
+            $(".activity.resource > a").each(function(a){
+                // Remove the onclick
+                this.removeAttribute("onclick");
+                // Psuedo-Direct link to the page. At least it's a 
+                // transparent redirect now.
+                var newurl = this.href.concat("&inpopup=true");
+                this.setAttribute("href", newurl);
+            });
+        }
         //
-        $(".activity.resource > a").each(function(a){
-            // Remove the onclick
-            this.removeAttribute("onclick");
-            // Psuedo-Direct link to the page. At least it's a 
-            // transparent redirect now.
-            var newurl = this.href.concat("&inpopup=true");
-            this.setAttribute("href", newurl);
-        });
-    }
-    //
-    // Psuedodirectlinkify all document URLs in resource listing
-    if (window.location.pathname == "/courses/mod/resource/index.php"){        
-        // Find all onclick elements in anchor links of such elements and
-        // remove them. Append &inpopup=true to all of them.
+        // Psuedodirectlinkify all document URLs in resource listing
+        if (window.location.pathname == "/courses/mod/resource/index.php"){        
+            // Find all onclick elements in anchor links of such elements and
+            // remove them. Append &inpopup=true to all of them.
 
-        $(".generaltable .c1 a").each(function(a){
-            // Remove the onclick
-            this.removeAttribute("onclick");
-            // Psuedo-Direct link to the page. At least it's a 
-            // transparent redirect now.
-            var newurl = this.href.concat("&inpopup=true");
-            this.setAttribute("href", newurl);
-        });
-    }
-
+            $(".generaltable .c1 a").each(function(a){
+                // Remove the onclick
+                this.removeAttribute("onclick");
+                // Psuedo-Direct link to the page. At least it's a 
+                // transparent redirect now.
+                var newurl = this.href.concat("&inpopup=true");
+                this.setAttribute("href", newurl);
+            });
+        }
+    });
 })(window, document, jQuery);
 
